a83b16832234cfc23bc55c1ba5043f3c764ab9dc
Andrew Owens <iseis360@gmail.com>
2016-02-05 00:38:57 -0800

    final

--- CONFIG.base.py
+++ CONFIG.base.py
@@ -11,5 +11,5 @@ DEBUG = True
 
 ### On ix.cs.uoregon.edu
 #SERVER_NAME = "ix.cs.uoregon.edu:{}".format(random.randint(5000,8000))
-PORT="{}".format(random.randint(5000,8000))
-DEBUG = False # Because it's unsafe to run outside localhost
+##PORT="{}".format(random.randint(5000,8000))
+##DEBUG = False # Because it's unsafe to run outside localhost
